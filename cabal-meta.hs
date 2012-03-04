16f64f6e2733120cd9fecba059d52c94d1809f52
Greg Weber <greg@gregweber.info>
2012-03-03 21:39:40 -0800

    upgrade to latest Text version of Shelly

--- cabal-meta.hs
+++ cabal-meta.hs
@@ -5,11 +5,12 @@
 -- support git & tar.gz urls
 
 import Shelly
+import Prelude hiding (FilePath)
+import Data.Monoid ((<>))
 import Data.Text.Lazy (Text, pack, unpack)
 import qualified Data.Text.Lazy as LT
 import Control.Monad (forM, forM_, when)
 import System.Environment (getArgs)
-import System.FilePath (takeExtension)
 
 source_file :: FilePath
 source_file = "sources.txt"
@@ -30,30 +31,29 @@ main = do
     forM_ pkgs echo
     _<-cabal_install pkgs
     forM_ pkgs $ \pkg ->
-      chdir (unpack pkg) $ "cabal-src-install" # ["--src-only"]
+      chdir pkg $ "cabal-src-install" # ["--src-only"]
     return ()
 
 readPackages :: Bool -> Text -> ShIO [Text]
 readPackages allowCabals dir = do
-  fullDir <- path (unpack dir)
+  fullDir <- path dir
   chdir fullDir $ do
     cabalPresent <- if allowCabals then return False else isCabalPresent
     if cabalPresent then return [] else do
         pkgs <- getSources
         child_pkgs <- forM pkgs $ \pkg -> do
-          b <- fmap (== fullDir) (path $ unpack pkg)
+          b <- fmap (== fullDir) (path pkg)
           if b  then return [] else readPackages False pkg
         return $ concatMap (\(p, ps) -> if null ps then [p] else ps) $ zip pkgs child_pkgs
   where
-    isCabalFile f = ".cabal" == takeExtension f
+    isCabalFile = LT.isSuffixOf ".cabal" 
     isCabalPresent = do
         cabals <- fmap (filter isCabalFile) $ ls "."
         return $ not $ null cabals 
 
+    terror = fail . unpack
     getSources :: ShIO [Text]
     getSources = do
         relative_pkgs <- fmap LT.lines $ readfile source_file
-        when (null relative_pkgs) $ error $ "empty " ++ source_file
-        mapM (liftAsString path) relative_pkgs
-      where
-        liftAsString f = fmap pack . f . unpack
+        when (null relative_pkgs) $ terror $ "empty " <> source_file
+        mapM path relative_pkgs
