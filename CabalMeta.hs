e8d5e9e99447c4a4e2971387b02c74d1caadfdc3
gregwebs <greg@gregweber.info>
2012-03-31 19:13:56 -0700

    cabal-meta 0.1

--- CabalMeta.hs
+++ CabalMeta.hs
@@ -8,10 +8,22 @@ module CabalMeta where
 import Shelly
 import Prelude hiding (FilePath)
 import Data.Text.Lazy (Text, unpack)
-import qualified Data.Text.Lazy as LT
-import Control.Monad (forM)
-import Data.Monoid (Monoid(..))
+import qualified Data.Text.Lazy as T
 import Filesystem.Path.CurrentOS (FilePath, hasExtension, filename)
+import Data.Maybe (fromMaybe, maybeToList)
+import Data.List (partition)
+
+#if __GLASGOW_HASKELL__ < 704
+import Data.Monoid (Monoid(..))
+import Control.Monad (when, forM)
+infixr 5 <>
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+#else
+import Control.Monad (forM)
+import Data.Monoid ((<>),Monoid(..))
+#endif
+
 {--
 import FileLocation (debug)
 --}
@@ -35,16 +47,21 @@ data Package = Directory {
 } | Package {
     pLocation :: Text
   , pFlags :: [Text]
+} | GitPackage {
+    gLocation :: Text
+  , pFlags :: [Text]
+  , gTag :: Maybe Text
 } deriving (Show, Eq)
 
 asList :: Package -> [Text]
 asList (Package l flags) = l:flags
+asList (GitPackage l flags tag) = l : flags ++ maybeToList tag
 asList (Directory d flags) = toTextUnsafe d : flags
 
 data PackageSources = PackageSources {
     dirs     :: [Package]
   , hackages :: [Package]
-  , https    :: [Package]
+  , https    :: [Package] -- also git for now 
   , gits     :: [Package]
 } deriving (Show, Eq)
 
@@ -67,6 +84,9 @@ instance Monoid PackageSources where
 vendor_dir :: FilePath
 vendor_dir = "vendor"
 
+git_ :: Text -> [Text] -> ShIO ()
+git_ = command1_ "git" []
+
 readPackages :: Bool ->  FilePath -> ShIO PackageSources
 readPackages allowCabals startDir = do
   fullDir <- path startDir
@@ -76,16 +96,20 @@ readPackages allowCabals startDir = do
         psources <- getSources
         when (psources == mempty) $ terror $ "empty " <>| source_file
 
-        let git_repos = map pLocation $ gits psources ++ https psources
-        child_vendor_pkgs <- if null git_repos then return [] else do
+        let git_pkgs = gits psources ++ https psources
+        child_vendor_pkgs <- if null git_pkgs then return [] else do
           mkdir_p vendor_dir
           chdir vendor_dir $
-            forM git_repos $ \repo -> do
+            forM git_pkgs $ \pkg -> do
+              let repo = gLocation pkg 
               let d = filename $ fromText repo
               e <- test_d d
-              if e
-                then run_ "git" ["clone", "--recursive", repo]
-                else return () -- pull & run git submodule update --init
+              if not e
+                then git_ "clone" ["--recursive", repo]
+                else chdir d $ git_ "fetch" ["origin"]
+              chdir d $ do
+                git_ "checkout" [fromMaybe "master" (gTag pkg)]
+                git_ "submodule" ["foreach", "git", "pull", "origin", "master"]
               readPackages False d
 
         child_dir_pkgs <- forM (dirs psources) $ \dir -> do
@@ -105,16 +129,17 @@ readPackages allowCabals startDir = do
                 zip (dirs psources ++ gits psources ++ https psources) (map dirs child_pkgs)
           }
   where
+    headMay [] = Nothing
+    headMay xs = Just (head xs)
+
     isCabalFile = flip hasExtension "cabal"
     isCabalPresent = fmap (any isCabalFile) (ls ".")
-
-    terror = fail . unpack
 
     getSources :: ShIO PackageSources
     getSources = do
         sourceContent <- readfile source_file
         let sources = paritionSources [ source | 
-              source <- map LT.words (LT.lines sourceContent),
+              source <- map T.words (T.lines sourceContent),
               "--" /= head source,
               not . null $ source
               ]
@@ -130,11 +155,15 @@ readPackages allowCabals startDir = do
           where
           go sources [] = sources
           go _ ([]:_) = error "impossible"
-          go sources ((name:flags):more) = let c = LT.head name in
-            if c == '.' || c == '/'               then go sources { dirs = mkDir: dirs sources } more
-              else if "http" `LT.isPrefixOf` name then go sources { https = mkPkg: https sources } more
-              else if "git:" `LT.isPrefixOf` name then go sources { gits = mkPkg: gits sources } more
-              else                                     go sources { hackages = mkPkg: hackages sources } more
+          go sources ((name:flags):more) = let c = T.head name in
+            if c == '.' || c == '/'              then go sources { dirs = mkDir: dirs sources } more
+              else if "http" `T.isPrefixOf` name then go sources { https = mkGit: https sources } more
+              else if "git:" `T.isPrefixOf` name then go sources { gits = mkGit: gits sources } more
+              else                                    go sources { hackages = mkPkg: hackages sources } more
             where
               mkDir = Directory (fromText name) flags
               mkPkg = Package name flags
+              mkGit = let (realFlags, tags) = partition (T.isPrefixOf "-") flags in
+                if length tags > 1
+                  then error $ unpack $ "did not understand" <> T.intercalate " " (asList (Package name flags))
+                  else GitPackage name realFlags (headMay tags)
